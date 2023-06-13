ab193867207bc959b74e1764b8eabb413aef7f00
PandaAsia <86446822+PandaAsia@users.noreply.github.com>
2023-06-12 22:55:59 -0500

    subir toda la univercidad

---
+++ universidad/js/app.js
@@ -0,0 +1,14 @@
+//localstorage
+localStorage.setItem('nombre', 'juan');
+
+//session storage
+
+//sessionStorage.setItem('nombre', 'juan');
+
+//eliminar 
+//localStorage.removeItem('nombre');
+
+const nombre=localStorage.getItem('nombre');
+console.log(nombre);
+
+localstorage.clear();
